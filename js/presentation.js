0724e6c11bd0d7abbbe10865dc2972bc7138eab8
Russulasp <109649978+Russulasp@users.noreply.github.com>
2026-08-11 20:38:04 +0900

    Build Reveal.js mushroom taxonomy presentation

---
+++ js/presentation.js
@@ -0,0 +1,13 @@
+Reveal.initialize({
+  hash: true,
+  history: true,
+  controls: true,
+  progress: true,
+  center: false,
+  width: 1600,
+  height: 900,
+  margin: 0,
+  transition: 'fade',
+  transitionSpeed: 'fast',
+  pdfSeparateFragments: false
+});
